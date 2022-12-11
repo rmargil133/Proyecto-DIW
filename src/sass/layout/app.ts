11ffb03e3f9572d1476881d4dfd48d6791811c73
Ruben Mariscal <92536400+rmargil133@users.noreply.github.com>
2022-12-11 21:00:15 +0100

    Delete src directory

--- src/sass/layout/app.ts
+++
@@ -1,8 +0,0 @@
-/* Cuando hago CLICK .button, .nav TOGGLE 'activo */
-
-const button = (document.querySelector('.button') as HTMLButtonElement)
-const nav = (document.querySelector('.nav') as HTMLElement)
-
-button.addEventListener('click' ,()=> {
-    nav.classList.toggle('activo')
-})
